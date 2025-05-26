d33eaa7dcb224389ad1da78717c7ed99ecf2b479
Viktor Kanishchev <kanishevv.dev@gmail.com>
2025-05-26 18:42:45 +0300

    Bump version to 0.0.33 and update package exports and sideEffects

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,19 @@ import { resolve } from 'path'
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import { libInjectCss } from 'vite-plugin-lib-inject-css'
+// import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts(), libInjectCss()],
+  plugins: [
+    vue(),
+    dts({
+      outDir: 'dist',
+      insertTypesEntry: true
+    }),
+    // libInjectCss()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -19,7 +26,8 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'Quicker.style',
-      fileName: (format) => `quicker.style.${format}.js`
+      fileName: (format) => `quicker.style.${format}.js`,
+      formats: ['es', 'umd']
     },
     rollupOptions: {
       external: ['vue'],
